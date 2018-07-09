9f871ca8fdd186568e761273950b2348a480bfa9
Jordan Bedrin <jordanbedrin@aol.com>
2018-07-09 17:07:50 -0400

    first commit

---
+++ app/assets/javascripts/todo_items.js
@@ -0,0 +1,6 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+alert("ENTER A TODO ITEM PLEASE!!!!!! :D");
+alert("hehehe u can't yet!!!");
+alert("wait..... NOW!!!");
